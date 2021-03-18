d508464c4ef289db36c31bed890acdc55aaa658d
hassanali1337 <shassana00@gmail.com>
2021-03-18 14:34:27 +0500

    initial commit

---
+++ shop_project/lib/widgets/product_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+
+
+class ProductItem  extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect( //ClipRect help clipping rectangle round corner
+    borderRadius:BorderRadius.circular(10),
+      child: GridTile( //Grid tile is use as header card for display image
+
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
+       footer: GridTileBar(  //gridtile is a textbar of product name
+         backgroundColor: Colors.black54,
+           leading: IconButton(  // leading make the icon left
+             icon: Icon(Icons.favorite),
+             color: Theme.of(context).accentColor,
+
+             onPressed: (){},
+           ),
+
+           trailing: IconButton(  //trailing make the icon right
+             icon: Icon(Icons.shopping_cart),
+             onPressed: (){},
+             color: Theme.of(context).accentColor,
+           ),
+           title: Text(
+             title,
+             textAlign: TextAlign.center,
+           ),
+
+       ),
+
+
+      ),
+    );
+  }
+}
