19575009e6a5b4d6ee6674d5b45d41e454e3f3c3
hassanali1337 <shassana00@gmail.com>
2021-03-19 16:35:48 +0500

    favorites product work

--- shop_project/lib/screens/products_overview_screen.dart
+++ shop_project/lib/screens/products_overview_screen.dart
@@ -1,16 +1,54 @@
 import 'package:flutter/material.dart';
 
+
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOption {
+  Favourites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+   var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOption selectedValue){
+              setState(() {
+                if (selectedValue == FilterOption.Favourites){
+                  _showOnlyFavorites = true;
+                }
+                else{
+                  _showOnlyFavorites = false;
+                }
+              });
+
+
+            },
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) =>[
+              PopupMenuItem(child: Text('Only favouite'), value: FilterOption.Favourites),
+              PopupMenuItem(child: Text('Show all'), value: FilterOption.All,),
+            ],
+          )
+        ],
       ),
 
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
+
     );
   }
 }
